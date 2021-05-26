31c555a7d8e971f3d02c117fae274b05fa6d7f23
Denis <gabden5545@gmail.com>
2021-05-27 00:58:53 +0300

    dasfsadf

---
+++ src/components/FileTree.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul class="directory-list">
+      <node-tree :node="root" @expand="changeExpand"></node-tree>
+    </ul>
+  </div>
+</template>
+
+<script>
+import NodeTree from "@/components/NodeTree.vue";
+export default {
+  components: {
+    NodeTree,
+  },
+  props: {
+    root: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    changeExpand(data) {
+      this.$emit("expand", data);
+    },
+  },
+};
+</script>
